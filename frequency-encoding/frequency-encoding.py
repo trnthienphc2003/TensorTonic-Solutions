be3a065d6f84d5855b26e1ec591ec3fa397b09e6
trnthienphc2003 <88192288+trnthienphc2003@users.noreply.github.com>
2026-05-03 14:11:10 +0700

    Update solution for frequency-encoding

--- frequency-encoding/frequency-encoding.py
+++ frequency-encoding/frequency-encoding.py
@@ -1,13 +1,8 @@
 def frequency_encoding(values):
-    """
-    Replace each value with its frequency proportion.
-    """
     n = len(values)
     cnt = {}
+    
     for v in values:
-        if v not in cnt.keys():
-            cnt.update({v : 1})
-        else:
-            cnt[v] += 1
+        cnt[v] = cnt.get(v, 0) + 1
 
     return [cnt[v] / n for v in values]
