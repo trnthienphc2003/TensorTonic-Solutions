116f03d7f3bc4e81b5bf21b6fa80762920839e27
trnthienphc2003 <88192288+trnthienphc2003@users.noreply.github.com>
2026-05-09 21:43:48 +0700

    Add solution for vit-class-token

---
+++ vit/vit-class-token/vit-class-token.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def prepend_class_token(patches: np.ndarray, embed_dim: int, cls_token: np.ndarray = None) -> np.ndarray:
+    """
+    Prepend learnable [CLS] token to patch sequence.
+    cls_token: shape (1, 1, D). If None, initialize randomly.
+    """
+    # YOUR CODE HERE
+    B, N, D = patches.shape
+    if cls_token is None:
+        cls_token = np.random.randn((1, 1, D)) * 0.01
+
+    cls_token = np.full((B, 1, D), cls_token[0])
+    patches = np.concatenate([cls_token, patches], axis=1)
+    return patches
