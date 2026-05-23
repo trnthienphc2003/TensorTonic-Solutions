5c4923ef09412b32c1f5ebe13d6d699ad4e106a2
trnthienphc2003 <88192288+trnthienphc2003@users.noreply.github.com>
2026-05-23 14:20:06 +0700

    Update solution for gini-impurity

--- gini-impurity/gini-impurity.py
+++ gini-impurity/gini-impurity.py
@@ -5,22 +5,23 @@ def gini_impurity(y_left, y_right):
     Compute weighted Gini impurity for a binary split.
     """
     # Write code here
+    assert y_left is not None and y_right is not None
     y_left, y_right = np.asarray(y_left), np.asarray(y_right)
     N_L, N_R = y_left.shape[0], y_right.shape[0]
     N = N_L + N_R
     if N == 0:
         return 0.
-    classes = np.unique(np.concatenate([y_left, y_right], axis=0))
-    def gini_node(y, classes):
-        if np.any(y):
-            y_onehot = y[..., np.newaxis] == classes[np.newaxis, ...]
-            y_perclass = np.average(y_onehot, axis=0)
-            return 1. - np.sum(y_perclass ** 2)
-        return 0.
+    # classes = np.unique(np.concatenate([y_left, y_right], axis=0))
+    def gini_node(y):
+        if y.shape[0] == 0:
+            return 0.
+        _, counts = np.unique(y, axis=0, return_counts=True)
+        prob = counts / y.shape[0]
+        return 1. - np.sum(prob ** 2)
 
     # assert False, y_left_perclasses
 
-    gini_left = gini_node(y_left, classes)
-    gini_right = gini_node(y_right, classes)
+    gini_left = gini_node(y_left)
+    gini_right = gini_node(y_right)
     return (N_L * gini_left + N_R * gini_right) / N
     # return None
