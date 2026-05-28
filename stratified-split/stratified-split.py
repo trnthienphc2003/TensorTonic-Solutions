096c4fa855c34c290adc9e43f88db72f2db64279
trnthienphc2003 <88192288+trnthienphc2003@users.noreply.github.com>
2026-05-28 12:51:38 +0700

    Update solution for stratified-split

--- stratified-split/stratified-split.py
+++ stratified-split/stratified-split.py
@@ -1,37 +1,37 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 def stratified_split(X, y, test_size=0.2, rng=None):
     """
-    Split features X and labels y into train/test while preserving class proportions.
+    Split X and y into train/test while preserving class proportions.
     """
-    # Write code here
-    # get class counts
     X = np.asarray(X)
     y = np.asarray(y)
-    unique_labels = np.unique(y)
-    num_classes = len(unique_labels)
-    train = []
-    test = []
-    N = len(X)
-    # if rng is None and seed:
-    #     rng = np.random.default_rng(seed)
-    for unique_label in unique_labels:
-        indices = np.where(y == unique_label)[0]
-        if rng is not None:
-            rng.shuffle(indices)
+
+    classes = np.unique(y)
+    num_classes = len(classes)
+    train_indices, test_indices = [], []
+    for cls in classes:
+        idx = np.where(y == cls)[0]
+        if rng is None:
+            np.random.shuffle(idx)
         else:
-            np.random.shuffle(indices)
-        # print(indices)
-        l = len(indices)
-        l_test = round(l * test_size)
-        l_train = l - l_test
-        # print(l_train)
-        if l_train == 0:
-            l_train = 1
-            l_test = l - 1
-        test.extend(indices[:l_test])
-        train.extend(indices[l_test:])
-    # print(train)
-    test.sort()
-    train.sort()
-    return X[train], X[test], y[train], y[test]
+            rng.shuffle(idx)
+
+        N = len(idx)
+        n_test = round(N * test_size)
+        n_train = N - n_test
+
+        if n_train == 0:
+            n_train = 1
+            n_test = N - 1
+
+        test_idx = idx[:n_test]
+        train_idx = idx[n_test:]
+
+        train_indices.extend(train_idx)
+        test_indices.extend(test_idx)
+
+    train_indices.sort()
+    test_indices.sort()
+    return X[train_indices], X[test_indices], y[train_indices], y[test_indices]
