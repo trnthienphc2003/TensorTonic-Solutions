bb9980ab96ea20d178cceb6c3535e25ba8dec5f7
trnthienphc2003 <88192288+trnthienphc2003@users.noreply.github.com>
2026-08-17 15:09:24 +0700

    Solve cumulative-returns

--- cumulative-returns/cumulative-returns.py
+++ cumulative-returns/cumulative-returns.py
@@ -4,6 +4,9 @@ def cumulative_returns(returns):
     """
     Compute the cumulative return at each time step.
     """
-    returns = [r + 1 for r in returns]
-    prod = accumulate(returns, lambda x, y: x * y)
-    return [p - 1 for p in prod]
+    out = []
+    cum = 1.
+    for r in returns:
+        cum *= (1. + r)
+        out.append(cum - 1)
+    return out
