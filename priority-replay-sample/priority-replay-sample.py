3d378435bd0bb5c7c5b422a589a50dc5f7ba4a83
trnthienphc2003 <88192288+trnthienphc2003@users.noreply.github.com>
2026-08-17 14:34:34 +0700

    Solve priority-replay-sample

--- priority-replay-sample/priority-replay-sample.py
+++ priority-replay-sample/priority-replay-sample.py
@@ -2,14 +2,14 @@ def priority_replay_sample(priorities, alpha, beta):
     """
     Compute sampling probabilities and importance sampling weights for PER.
     """
-    pow_p = list(map(lambda p: p ** alpha, priorities))
+    pow_p = [p ** alpha for p in priorities]
     sum_pow_p = sum(pow_p)
-    probs = list(map(lambda p: p / sum_pow_p, pow_p))
+    probs = [p / sum_pow_p for p in pow_p]
     # assert False, probs
 
     N = len(priorities)
-    weights = list(map(lambda p: (N * p) ** (-beta), probs))
+    weights = [1. / (N * p) ** beta for p in probs]
     mx_weights = max(weights)
-    norm_w = list(map(lambda w: w / mx_weights, weights))
+    norm_w = [w / mx_weights for w in weights]
     
     return [probs, norm_w]
